3e32566b68c3c089481d7fc245db9426a0fbfd86
Arif Suryo Kurniawan <jayadewatabagaskara@gmail.com>
2023-10-25 09:38:29 +0700

    second commit

--- resources/views/layouts/navbarLogin.blade.php
+++ resources/views/layouts/navbarLogin.blade.php
@@ -7,14 +7,24 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
 </head>
+
+<style>
+    .borderNavbarRight{
+        border: 1px solid #000; 
+        padding: 5px 10px; 
+        border-radius: 5px;
+        text-decoration: none;
+    }
+</style>
+
 <body>
 
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
     <a class="navbar-brand" href="#">ElliteBid.com</a>
     <ul class="nav navbar-nav navbar-right">
-      <li><a href="#"> Sign Up </a></li>
-      <li><a href="#"> Login </a></li>
+      <li><a href="#" class="borderNavbarRight" style="margin-right: 10px;"> Sign Up</a></li>
+      <li><a href="#" class="borderNavbarRight"> Login </a></li>
     </ul>
     </div>
     </div>
