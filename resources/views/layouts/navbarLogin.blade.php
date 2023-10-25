6f856978d14da1f3221d074d7c96fe00b06cbc33
Arif Suryo Kurniawan <jayadewatabagaskara@gmail.com>
2023-10-25 09:31:51 +0700

    first commit

---
+++ resources/views/layouts/navbarLogin.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+</head>
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+    <a class="navbar-brand" href="#">ElliteBid.com</a>
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="#"> Sign Up </a></li>
+      <li><a href="#"> Login </a></li>
+    </ul>
+    </div>
+    </div>
+    </nav>
+
+    <div class="container-mt4">
+        @yield('container')
+    </div>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-
+    HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+</body>
+</html>
